01074d4c866cd84d584f19af55598b2ba01bcdb8
JPFeng <jpfeng-dev@proton.me>
2023-10-22 12:43:01 -0600

    Added language switcher component to pages

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,14 @@
 <template>
   <div>
-    <div style="display: flex;align-items: center;justify-content: space-between;">
-      <h1>{{ $t('index') }}</h1>
-
-      <div style="display: flex; column-gap: 1rem;">
-        <NuxtLinkLocale to="/about">{{ $t('about') }}</NuxtLinkLocale>
+  <div style="display: flex;align-items: center;justify-content: space-between;">
+    <h1>{{ $t('index') }}</h1>
+    <div style="display: flex; column-gap: 1rem;">
+      <NuxtLinkLocale to="/about">{{ $t('about') }}</NuxtLinkLocale>
       </div>
   </div>
+
+  <footer>
+    <LanguageSwitcher />
+  </footer>
   </div>
 </template>
