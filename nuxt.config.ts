d2d89cddc35f2da6ade61f3d471b568063ca97cc
JPFeng <jpfeng-dev@proton.me>
2023-10-22 12:29:48 -0600

    Added some i18n config props

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,16 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import type { NuxtI18nOptions } from "@nuxtjs/i18n/dist/module"
+
+const DEFAULT_LOCALE = 'en-us'
+
+const i18n: NuxtI18nOptions = {
+  lazy: true,
+  defaultLocale: DEFAULT_LOCALE,
+  strategy: "prefix_except_default"
+}
+
 export default defineNuxtConfig({
   modules: ['@nuxtjs/i18n'],
+  i18n,
   devtools: { enabled: true }
 })
