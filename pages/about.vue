eff3057c857716912443ed2002e178d1e3503846
JPFeng <jpfeng-dev@proton.me>
2023-10-22 12:41:31 -0600

    Updated pages to use i18n translations

--- pages/about.vue
+++ pages/about.vue
@@ -1,5 +1,10 @@
 <template>
   <div>
-    <h1>About</h1>
+    <div style="display: flex;align-items: center;justify-content: space-between;">
+      <h1>{{ $t('about') }}</h1>
+      <div style="display: flex; column-gap: 1rem;">
+        <NuxtLinkLocale to="/">{{ $t('index') }}</NuxtLinkLocale>
+      </div>
+    </div>
   </div>
 </template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,11 @@
 <template>
   <div>
-    <h1>Index</h1>
+    <div style="display: flex;align-items: center;justify-content: space-between;">
+      <h1>{{ $t('index') }}</h1>
+
+      <div style="display: flex; column-gap: 1rem;">
+        <NuxtLinkLocale to="/about">{{ $t('about') }}</NuxtLinkLocale>
+      </div>
+  </div>
   </div>
 </template>
