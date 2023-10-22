ea3f077d46adf7b33dbea3063f745fa2efed83f2
JPFeng <jpfeng-dev@proton.me>
2023-10-22 12:31:09 -0600

    Added locales json files and more config props

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,30 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 import type { NuxtI18nOptions } from "@nuxtjs/i18n/dist/module"
+import type { LocaleObject } from "@nuxtjs/i18n/dist/runtime/composables"
 
 const DEFAULT_LOCALE = 'en-us'
 
+const locales: LocaleObject[]  = [
+  {
+    code: 'en-us',
+    name: 'English (US)',
+    iso: 'en-us',
+    file: 'en-us.json'
+  },
+  {
+    code: 'es-cr',
+    name: 'Español (Costa Rica)',
+    iso: 'es-cr',
+    file: 'es-cr.json'
+  }
+]
+
 const i18n: NuxtI18nOptions = {
   lazy: true,
+  langDir: 'locales/',
   defaultLocale: DEFAULT_LOCALE,
-  strategy: "prefix_except_default"
+  strategy: "prefix_except_default",
+  locales
 }
 
 export default defineNuxtConfig({
